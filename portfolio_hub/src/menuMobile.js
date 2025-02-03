39e91bfd8e6784aaab3e6addc2a1b213220585c7
viniciogomes92 <viniciogomes@gmail.com>
2025-02-03 17:38:29 -0300

    Update de novos projetos na página.

--- portfolio_hub/src/menuMobile.js
+++ portfolio_hub/src/menuMobile.js
@@ -1,4 +1,10 @@
 const hamburguer = document.querySelector('.hamburguer');
+const menuAbout = document.querySelector('.menu-about');
+const menuProjects = document.querySelector('.menu-projects');
+const menuContact = document.querySelector('.menu-contact');
 const nav = document.querySelector('.nav');
 
 hamburguer.addEventListener('click', () => nav.classList.toggle('active'));
+menuAbout.addEventListener('click', () => nav.classList.toggle('active'));
+menuProjects.addEventListener('click', () => nav.classList.toggle('active'));
+menuContact.addEventListener('click', () => nav.classList.toggle('active'));
