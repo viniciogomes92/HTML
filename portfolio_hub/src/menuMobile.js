4eb1ad67d9f88a799bb891c41e915aa43d3730e0
viniciogomes92 <viniciogomes@gmail.com>
2025-02-04 20:08:37 -0300

    Animated features included.

--- portfolio_hub/src/menuMobile.js
+++ portfolio_hub/src/menuMobile.js
@@ -4,9 +4,22 @@ const menuAbout = document.querySelector('.menu-about');
 const menuProjects = document.querySelector('.menu-projects');
 const menuContact = document.querySelector('.menu-contact');
 const nav = document.querySelector('.nav');
+const hiddenDivs = document.querySelectorAll('.hidden');
 
 hamburguer.addEventListener('click', () => nav.classList.toggle('active'));
 menuHome.addEventListener('click', () => nav.classList.toggle('active'));
 menuAbout.addEventListener('click', () => nav.classList.toggle('active'));
 menuProjects.addEventListener('click', () => nav.classList.toggle('active'));
 menuContact.addEventListener('click', () => nav.classList.toggle('active'));
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  })
+});
+
+hiddenDivs.forEach((hiddenDiv) => observer.observe(hiddenDiv));
