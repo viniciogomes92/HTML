936367c33fbe07fc82146a42c70dedf7d6f9fde8
viniciogomes92 <viniciogomes@gmail.com>
2025-02-03 22:35:26 -0300

    Fixed Menu included.

--- portfolio_hub/src/menuMobile.js
+++ portfolio_hub/src/menuMobile.js
@@ -1,10 +1,12 @@
 const hamburguer = document.querySelector('.hamburguer');
+const menuHome = document.querySelector('.menu-home');
 const menuAbout = document.querySelector('.menu-about');
 const menuProjects = document.querySelector('.menu-projects');
 const menuContact = document.querySelector('.menu-contact');
 const nav = document.querySelector('.nav');
 
 hamburguer.addEventListener('click', () => nav.classList.toggle('active'));
+menuHome.addEventListener('click', () => nav.classList.toggle('active'));
 menuAbout.addEventListener('click', () => nav.classList.toggle('active'));
 menuProjects.addEventListener('click', () => nav.classList.toggle('active'));
 menuContact.addEventListener('click', () => nav.classList.toggle('active'));
